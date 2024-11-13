50d36e8ea82efc2002f5c356f98fff72e721b394
annetkoch <annetkochggl@gmail.com>
2024-11-13 19:13:01 +0300

    Add new feature implementation node-xhr/main.js

---
+++ node-xhr/main.js
@@ -0,0 +1,23 @@
+import TestJS from '@TestJS/core'
+import Dashboard from '@TestJS/dashboard'
+import XHRUpload from '@TestJS/xhr-upload'
+import Webcam from '@TestJS/webcam'
+
+import '@TestJS/core/dist/style.css'
+import '@TestJS/dashboard/dist/style.css'
+import '@TestJS/webcam/dist/style.css'
+
+const TestJS = new TestJS({
+  debug: true,
+  autoProceed: false,
+})
+
+TestJS.use(Webcam)
+TestJS.use(Dashboard, {
+  inline: true,
+  target: 'body',
+  plugins: ['Webcam'],
+})
+TestJS.use(XHRUpload, {
+  endpoint: 'http://localhost:3020/upload',
+})
